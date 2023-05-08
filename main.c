a2e28522166b13c9337819602f039e62f578020d
bensbahou <younesbensbahou@gmail.com>
2023-05-08 13:35:40 +0100

    create infinite loop

--- main.c
+++ main.c
@@ -6,9 +6,19 @@ int main(int ac, char **argv)
 	char *lineptr;
 	size_t n = 0;
 
-	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
+	/* declaring void variables */
+	(void)ac;
+	(void)argv;
 
-	free(lineptr);
+	/* create an infinite loop */
+	while (1)
+	{
+		printf("%s", prompt);
+		getline(&lineptr, &n, stdin);
+		printf("%s\n", lineptr);
+
+		/* free up allocated memory */
+		free(lineptr);
+	}
 	return (0);
 }
